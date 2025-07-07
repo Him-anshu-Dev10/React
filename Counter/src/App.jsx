d186e41b075fed6b7278fa106b66cfe497759587
Him-anshu-Dev10 <himutech10@gmail.com>
2025-07-07 21:28:16 +0530

    this is a counter program

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -4,11 +4,26 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  let [counter, setCounter] = useState(15);
+  // let counter = 12;
+  const addValue = () => {
+    console.log("clicked", counter);
+    counter += 1;
+    setCounter(counter);
+  };
 
+  const removeValue = () => {
+    console.log("clicked", counter);
+    counter -= 1;
+    setCounter(counter);
+  };
   return (
     <>
       <h1>Chai and Himu</h1>
+      <h2>counter value:{counter} </h2>
+      <button onClick={addValue}>addValue</button>
+      <br />
+      <button onClick={removeValue}>removeValue</button>
     </>
   );
 }
